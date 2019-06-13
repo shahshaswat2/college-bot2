7ff0dc5c261059108b2a97bbd1ac28d62b792a7c
shahshaswat2 <shahshaswat2@gmail.com>
2019-06-13 19:19:52 +0530

    Main App to run

--- newapp.py
+++ newapp.py
@@ -2,20 +2,28 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
+# creating a class
+
 
 class ParulBot:
     def __init__(self, username, password):
+        # constructor
         self.username = username
         self.password = password
         self.driver = webdriver.Firefox()
 
+    # closing of browser is not necessary but still if you want to..
     def closeBrowser(self):
         self.driver.close()
 
+    #   login function
     def login(self):
+        # calling our driver to open firefox
         driver = self.driver
         driver.get("http://180.211.119.163:8081/StudentPanel/")
         time.sleep(2)
+        # read the selenimum documentation to finding/locating particular text box, elements, links etc
+        # here we use xpath method to find username and password box
         user_element = driver.find_element_by_xpath(
             "//input[@name='txtEnrollmentNo']")
         user_element.clear()
@@ -27,6 +35,7 @@ class ParulBot:
         password_element.send_keys(Keys.RETURN)
         time.sleep(2)
 
+    # to go to attendance page after signing in
     def attendance(self):
         driver = self.driver
         driver.get(
